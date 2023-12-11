2fc63ba06a16e86507e79a042a590ce6e137b01e
ekasapta1997 <anggaputrach89@gmail.com>
2023-12-11 14:35:40 +0700

    Commit 2

--- Kode delete data pns.cpp
+++ Kode delete data pns.cpp
@@ -5,6 +5,55 @@ using namespace std;
 string n[max], nip[max], u[max], jk[max], s[max], pend[max], pk[max], tt[max], gol[max], g[max], loop;
 int pos=0;
 
+void edit(){
+	int y, k;
+	do{
+	
+		cout << "Edit Data ke : ";
+		cin>>y;
+		cout<<"Edit Nomor : ";
+		cin>>k;
+		cin.ignore();
+		if(k==1){
+		cout<<"Edit Nama : ";
+		getline(cin, n[y-1]);
+		}else if(k==2){
+		cout<<"Edit NIP : ";
+		getline(cin, nip[y-1]);
+		}else if(k==3){
+		cout<<"Edit Umur : ";
+		getline(cin, u[y-1]);
+		}else if(k==4){
+		cout<<"Edit Jenis Kelamin : ";
+		getline(cin, jk[y-1]);
+		}else if(k==5){
+		cout<<"Edit Status : ";
+		getline(cin, s[y-1]);
+		}else if(k==6){
+		cout<<"Edit Pendidikan : ";
+		getline(cin, pend[y-1]);
+		}else if(k==7){
+		cout<<"Edit Pekerjaan : ";
+		getline(cin, pk[y-1]);
+		}else if(k==8){
+		cout<<"Edit Tempat Tugas : ";
+		getline(cin, tt[y-1]);
+		}else if(k==9){
+		cout<<"Edit Golongan : ";
+		getline(cin, gol[y-1]);
+		}else if(k==10){
+		cout<<"Edit Gaji : ";
+		getline(cin, g[y-1]);
+		}else{
+			cout<<"selesai";
+		}
+		cout<<"Edit menjadi : ";
+		
+		cout << "ubah lagi (y/t) : ";
+		cin >> loop;
+	} while (loop == "y");
+}
+
 void del(){
 	int x;
 	do{
@@ -29,11 +78,15 @@ int main(){
 	int pil;
 	do{
 		system("cls");
+		cout<<"3. Edit Data\n"
 		cout<<"4. Hapus Data\n";
 	
 		cout<<"Pilihan : ";
 		cin>>pil;
 		switch(pil){
+		case 3:
+			edit();
+			break;
 		case 4:
 			del();
 			break;
