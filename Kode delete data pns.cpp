485fa6a247b4f9f098b4ea73ce838b495ad3b315
unknown <deliairyanti07@gmail.com>
2023-12-11 14:43:23 +0700

    Commit 3

--- Kode delete data pns.cpp
+++ Kode delete data pns.cpp
@@ -5,6 +5,44 @@ using namespace std;
 string n[max], nip[max], u[max], jk[max], s[max], pend[max], pk[max], tt[max], gol[max], g[max], loop;
 int pos=0;
 
+void add(){
+
+	do{
+		if(pos<max){
+			cin.ignore();
+			cout<<"Nama : ";
+			getline(cin, n[pos]);
+			cout<<"NIP : ";
+			getline(cin, nip[pos]);
+			cout<<"Usia : ";
+			getline(cin, u[pos]);
+			cout<<"Jenis Kelamin : ";
+			getline(cin, jk[pos]);
+			cout<<"Status : ";
+			getline(cin, s[pos]);
+			cout<<"Pendidikan : ";
+			getline(cin, pend[pos]);
+			cout<<"Pekerjaan : ";
+			getline(cin, pk[pos]);
+			cout<<"Tempat Tugas : ";
+			getline(cin, tt[pos]);
+			cout<<"Golongan : ";
+			getline(cin, gol[pos]);
+			cout<<"Gaji : ";
+			getline(cin, g[pos]);
+			pos++;
+			cout<<"ulangi (y/t) : ";
+			cin>>loop;
+		}else{
+			cout<<"memori penuh"<<endl;
+			cout<<"isi t untuk kembali";
+			cin>>loop;
+		}
+	
+	}while(loop=="y");
+	cout<<"Tambahkan Data"<<endl;
+}
+
 void edit(){
 	int y, k;
 	do{
@@ -78,12 +116,16 @@ int main(){
 	int pil;
 	do{
 		system("cls");
-		cout<<"3. Edit Data\n"
+		cout<<"1. Tambah Data\n";
+		cout<<"3. Edit Data\n";
 		cout<<"4. Hapus Data\n";
 	
 		cout<<"Pilihan : ";
 		cin>>pil;
 		switch(pil){
+		case 1:
+			add();
+			break;
 		case 3:
 			edit();
 			break;
